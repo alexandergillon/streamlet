44d85020a51ac9873ead98909653bafc7a903075
Alex Gillon <alexandergillon@outlook.com>
2023-09-01 13:36:26 -0400

    Adds a message class which represents messages to be included in the blockchain, and associated unit tests.

---
+++ node/src/main/java/com/github/alexandergillon/streamlet/node/models/Message.java
@@ -0,0 +1,89 @@
+package com.github.alexandergillon.streamlet.node.models;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.TimeZone;
+
+/** POJO to represent a message, sent by users to be included in the blockchain. */
+@Getter
+@EqualsAndHashCode
+public class Message {
+
+    /** The username of the user who sent this message. */
+    private final String username;
+
+    /** Message text. */
+    private final String text;
+
+    /** Timestamp of the message. Used for de-duplication. All timestamps are truncated to second-level accuracy. */
+    private final long timestamp;
+
+    /**
+     * Constructor. NOTE: all timestamps are truncated to second-level accuracy.
+     * @throws IllegalArgumentException If username contains a colon character.
+     * */
+    public Message(String username, String text, long timestamp) throws IllegalArgumentException {
+        if (username.contains(":")) throw new IllegalArgumentException("Username " + username + " cannot contain ':'");
+
+        this.username = username;
+        this.text = text;
+        // truncates timestamp to seconds
+        this.timestamp = Instant.ofEpochMilli(timestamp).truncatedTo(ChronoUnit.SECONDS).toEpochMilli();
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return simpleDateFormat.format(new Date(timestamp)) + " | " + username + ": " + text;
+    }
+
+    /** Converts this message to its string representation, then converts that string to ASCII bytes. */
+    public byte[] toStringBytes() {
+        return toString().getBytes(StandardCharsets.US_ASCII);
+    }
+
+    /**
+     * Constructs a {@link Message} from its string representation.
+     *
+     * @param string The string representation of a {@link Message}.
+     * @return That string, converted to a {@link Message}.
+     * @throws ParseException If the string representation is invalid.
+     */
+    public static Message fromString(String string) throws ParseException {
+        int barIndex = string.indexOf("|");
+        int colonIndex = barIndex + string.substring(barIndex).indexOf(":");
+        String dateString = string.substring(0, barIndex);
+        String username = string.substring(barIndex + 2, colonIndex);
+        String message = string.substring(colonIndex + 2);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        long timestamp = simpleDateFormat.parse(dateString).getTime();
+
+        return new Message(username, message, timestamp);
+    }
+
+    /**
+     * Converts an ASCII-encoded string (given as a byte array) to a string, and then converts that string to a {@link Message}.
+     *
+     * @param bytes ASCII-encoded string, as a byte array.
+     * @return That string, converted to a {@link Message}.
+     * @throws ParseException If the string representation is invalid.
+     */
+    public static Message fromStringBytes(byte[] bytes) throws ParseException {
+        String string = new String(bytes, StandardCharsets.US_ASCII);
+        return fromString(string);
+    }
+
+
+
+}
---
+++ node/src/test/java/com/github/alexandergillon/streamlet/node/models/MessageTest.java
@@ -0,0 +1,118 @@
+package com.github.alexandergillon.streamlet.node.models;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.TimeZone;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageTest {
+
+    // Tests that colons are not allowed in usernames
+    @Test
+    public void testNoColonInUsername() {
+        assertThrows(IllegalArgumentException.class, () -> new Message("illegal:username", "message", 0));
+    }
+
+    // Tests truncation of timestamps to second-level accuracy
+    @RepeatedTest(50)
+    public void testTimestampTruncation() {
+        String username = UUID.randomUUID().toString();
+        String messageText = UUID.randomUUID().toString();
+        long nowSeconds = Instant.now().getEpochSecond();
+        long nowMillis = secondsToMillis(nowSeconds);
+
+        Message message1 = new Message(username, messageText, nowMillis);
+        assertEquals(nowMillis, message1.getTimestamp());
+        Message message2 = new Message(username, messageText, nowMillis+1);
+        assertEquals(nowMillis, message2.getTimestamp());
+        Message message3 = new Message(username, messageText, nowMillis+100);
+        assertEquals(nowMillis, message3.getTimestamp());
+        Message message4 = new Message(username, messageText, nowMillis + ThreadLocalRandom.current().nextInt(50, 150));
+        assertEquals(nowMillis, message4.getTimestamp());
+
+        assertEquals(message1, message2);
+        assertEquals(message2, message3);
+        assertEquals(message3, message4);
+    }
+
+    @RepeatedTest(50)
+    public void testToString() {
+        long timestamp = dateStringToMillisRandomOffsetUTC("2013/03/17 18:34");
+        Message message = new Message("sdnjajhu3yuy3", "msesgafdhbdevye", timestamp);
+
+        assertEquals(message.toString(), "2013/03/17 18:34 | sdnjajhu3yuy3: msesgafdhbdevye");
+    }
+
+    @RepeatedTest(50)
+    public void testFromString() throws ParseException {
+        Message message = Message.fromString("2017/05/23 15:19 | sadubeuvvfe: psadoeine");
+        assertEquals(message.getUsername(), "sadubeuvvfe");
+        assertEquals(message.getText(), "psadoeine");
+        assertEquals(message.getTimestamp(), dateStringToMillisUTC("2017/05/23 15:19"));
+    }
+
+    @RepeatedTest(50)
+    public void testToStringBytes() {
+        long timestamp = dateStringToMillisRandomOffsetUTC("2013/03/17 18:34");
+        Message message = new Message("sdnjajhu3yuy3", "msesgafdhbdevye", timestamp);
+
+        assertArrayEquals(message.toStringBytes(), "2013/03/17 18:34 | sdnjajhu3yuy3: msesgafdhbdevye".getBytes(StandardCharsets.US_ASCII));
+    }
+
+    @RepeatedTest(50)
+    public void testFromStringBytes() throws ParseException {
+        Message message = Message.fromStringBytes("2017/05/23 15:19 | sadubeuvvfe: psadoeine".getBytes(StandardCharsets.US_ASCII));
+        assertEquals(message.getUsername(), "sadubeuvvfe");
+        assertEquals(message.getText(), "psadoeine");
+        assertEquals(message.getTimestamp(), dateStringToMillisUTC("2017/05/23 15:19"));
+    }
+
+    @RepeatedTest(50)
+    public void testToStringFromString() throws ParseException {
+        String username = UUID.randomUUID().toString();
+        String messageText = UUID.randomUUID().toString();
+        long timestamp = dateStringToMillisRandomOffsetUTC("1987/06/22 04:21");
+
+        Message message = new Message(username, messageText, timestamp);
+        assertEquals(message, Message.fromString(message.toString()));
+    }
+
+    @RepeatedTest(50)
+    public void testToStringFromStringBytes() throws ParseException {
+        String username = UUID.randomUUID().toString();
+        String messageText = UUID.randomUUID().toString();
+        long timestamp = dateStringToMillisRandomOffsetUTC("1987/06/22 04:21");
+
+        Message message = new Message(username, messageText, timestamp);
+        assertEquals(message, Message.fromStringBytes(message.toStringBytes()));
+    }
+
+
+    private long secondsToMillis(long seconds) {
+        return seconds * 1000L;
+    }
+
+    private long dateStringToMillisUTC(String date) {
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+            simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            return simpleDateFormat.parse(date).getTime();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private long dateStringToMillisRandomOffsetUTC(String date) {
+        return dateStringToMillisUTC(date) + ThreadLocalRandom.current().nextInt(100, 800);
+    }
+
+}
