6b01056514fbb4b31fe1267c36e82effa8284958
Alex Gillon <alexandergillon@outlook.com>
2023-09-16 12:32:41 -0400

    Adds GNU GPL disclaimer to all Java source files.

--- broadcast/src/test/java/com/github/alexandergillon/streamlet/broadcast/controllers/PayloadControllerTest.java
+++ broadcast/src/test/java/com/github/alexandergillon/streamlet/broadcast/controllers/PayloadControllerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023 Alexander Gillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.github.alexandergillon.streamlet.broadcast.controllers;
 
 import com.github.alexandergillon.streamlet.broadcast.services.KafkaService;
@@ -9,7 +25,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
